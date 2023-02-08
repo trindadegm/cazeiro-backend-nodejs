00643ef66fe29043f13febd42c55edca25fd45f8
Gustavo Moitinho Trindade <gmoitinhotrindade@gmail.com>
2023-02-08 19:09:16 -0300

    Add 'got' as a dependency to use when testing

--- tests/integration.js
+++ tests/integration.js
@@ -1,8 +1,11 @@
 import test from 'node:test';
 import assert from 'node:assert';
+import got from 'got';
 
 test("Create Nota Fiscal", async t => {
-    await t.test("Example", t => {
+    await t.test("Example", async t => {
         console.log("Example");
+        let test = await got.get('http://localhost:3080');
+        assert(test.statusCode == 200, "Test request failed");
     });
 });
